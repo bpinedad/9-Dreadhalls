922ac3c6236065b1d4291fe5f1d4e4e25c712be9
bpinedad <bpinedad96@gmail.com>
2023-04-03 19:45:17 -0400

    Initial code

---
+++ Assets/Scripts/FallDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDetector : MonoBehaviour {
+
+	public float maxFall = -1f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// If player is below certain Y limit go to game over screen
+		if (transform.position.y < maxFall) {
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
